abc50540d1ff922d8085db134519d694815d036d
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2025-07-23 18:36:48 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// File: next.config.js
+module.exports = {
+  // ... altre configurazioni ...
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
+};
