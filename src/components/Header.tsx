0d9931d3096cf1fbd6b59773e0692930629d2ae1
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2025-07-27 01:23:36 +0200

    Delete src/components/Header.tsx

--- src/components/Header.tsx
+++
@@ -1,5 +0,0 @@
-// File: src/components/Header.tsx
-<Link href="/bridge" className="flex items-center gap-2">
-  <ArrowsRightLeftIcon className="h-5 w-5" />
-  <span>Cross-Chain Swap</span>
-</Link>
